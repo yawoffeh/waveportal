20848837c6193f6449a1d040d0e305cfd904e9d3
Red <yawoffeh123@gmail.com>
2022-06-04 14:00:37 +0000

    update hello contract

--- contracts/hello.sol
+++ contracts/hello.sol
@@ -5,7 +5,20 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract Hello {
+	uint256 totalWaves;
+
 	constructor() {
 	    console.log("Hello world");
 	}
+
+	function wave() public {
+		totalWaves += 1;
+		console.log("%s has just waved!", msg.sender);
+	}
+
+	function totalWaves() public view returns (uint256) {
+		console.log("We have %d total waves", totalWaves);
+		return totalWaves;
+	}
+
 }
--- scripts/hello.js
+++ scripts/hello.js
@@ -1,11 +1,22 @@
 
 const main = async () => {
+	const owner = await hre.ethers.getSigners();
 	const helloContractFactory = await hre.ethers.getContractFactory("Hello");
 	const helloContract = await helloContractFactory.deploy();
 
 	helloContract.deployed();
 
 	console.log("hello deployed to ", helloContract.address);
+	console.log("Contract deployed by", owner.address)
+
+	let waveCount;
+	waveCount = await helloContract.totalWaves();
+
+	let wave;
+	wave = await helloContract.wave();
+	await wave.wait();
+
+	waveCount = await helloContract.totalWaves();
 };
 
 
